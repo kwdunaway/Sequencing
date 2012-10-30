7c5a4f40f46894837c8ba28cb09990cc8cd97d96
Keith <kwdunaway@ucdavis.edu>
2012-10-30 11:16:11 -0700

    Created SeqProcess.pm and gave it outline

---
+++ scripts/SeqProcess.pm
@@ -0,0 +1,24 @@
+use strict; use warnings;
+
+###############################################################################################
+# Author: Keith Dunaway & Roy Chu
+# Email: kwdunaway@ucdavis.edu $ rgchu@ucdavis.edu
+# Date: 10-30-2012
+# Module Name: SeqProcess.pm
+#
+# This is a module with sequencing processing commands.
+#
+################################################################################################
+
+# Combine and filter zipped files
+# Run Bowtie
+# Separate into 3 different sets (uniq, nonuniq, unaligned)
+# ElandExt to BED
+# BED to WIG
+# WIG to FPKMWIG
+# Vis FPKMWIG
+
+
+
+1;
+
